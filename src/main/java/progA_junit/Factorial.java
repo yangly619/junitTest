415974058e3b2b971c168f4748939e375fdd85f4
[yangyang] <[yangyangli@uma.es]>
2019-04-26 12:24:58 +0200

    2

--- src/main/java/progA_junit/Factorial.java
+++ src/main/java/progA_junit/Factorial.java
@@ -2,23 +2,27 @@ package progA_junit;
 
 /**
  * Hello world!
- *
  */
-public class Factorial
-{
-     public int compute(int number){
-         int r;
-         if(number==0){
-            r=1;
-         }else if(number==1){
-             r=1;
-         }else {
-             r=number*compute(number-1);
-         }
+public class Factorial {
 
-
-
-         return r;
+  public double compute(int number) {
+    double r;
+    if (number < 0) {
+      throw new RuntimeException("valor es negativo");
+    } else if ((number == 0) || (number == 1)) {
+      r = 1;
+    } else {
+      r = number * compute(number - 1);
     }
+    return r;
+  }
+
+  public double addNumberList(int number) {
+    int a = 5;
+    for (int III = 0; III < 5; III++) {
+      a = a + 1;
+    }
+    return a;
+  }
 
 }
--- src/test/java/progA_junit/FactorialTest.java
+++ src/test/java/progA_junit/FactorialTest.java
@@ -1,5 +1,7 @@
 package progA_junit;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,13 +9,50 @@ import static org.junit.Assert.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class FactorialTest
-{
-    Factorial fact=new Factorial();
-    int expected=24;
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertEquals( expected,fact.compute(4) );
-    }
+public class FactorialTest {
+  private static final double epsilon=0.0001;
+  Factorial factorial;
+
+  @Before
+  public void setup() {
+    factorial = new Factorial();
+  }
+
+  @After
+  public void after() {
+    factorial = null;
+  }
+
+  @Test
+  public void should0return1() {
+    double expected = 1;
+    double obtein= factorial.compute(0);
+    assertEquals(expected, obtein,epsilon);
+  }
+
+  @Test
+  public void should1return1() {
+    double expected = 1;
+    double obtein= factorial.compute(1);
+    assertEquals(expected, obtein,epsilon);
+  }
+
+  @Test
+  public void should2return2() {
+    double expected = 2;
+    double obtein= factorial.compute(2);
+    assertEquals(expected, obtein,epsilon);
+  }
+
+  @Test
+  public void should3return6() {
+    double expected = 6;
+    double obtein= factorial.compute(3);
+    assertEquals(expected, obtein,epsilon);
+  }
+  @Test(expected = RuntimeException.class)
+  public void shouldFactorialThrowExceptionIfValueIsNegative() {
+    factorial.compute(-2);
+  }
+
 }
